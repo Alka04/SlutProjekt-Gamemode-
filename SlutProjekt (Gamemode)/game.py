199bda49bd5bea59f1fc3e038adec91373c13aaa
Alka04 <89245759+Alka04@users.noreply.github.com>
2022-05-03 15:37:12 +0200

    Granater samt kommentarer på svenska

--- SlutProjekt (Gamemode)/game.py
+++ SlutProjekt (Gamemode)/game.py
@@ -13,24 +13,28 @@ SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Shooter')
 
-#set framerate
+#bestäm framerate
 clock = pygame.time.Clock()
 FPS = 60
 
-#define game variables
+#definera spelets variabler
 GRAVITY = 0.75
 
-#define player action variables
+#definera spelarens handling variabler
 moving_left = False
 moving_right = False
 shoot = False
+grenade = False
+grenade_thrown = False
 
-#load images
-#bullet
+#ladda in bilder
+#kula
 bullet_img = pygame.image.load('img/icons/bullet.png').convert_alpha()
+#granat
+grenade_img = pygame.image.load('img/icons/grenade.png').convert_alpha()
 
 
-#define colours
+#definera färger
 BG = (144, 201, 120)
 RED = (255, 0, 0)
 
@@ -42,7 +46,7 @@ def draw_bg():
 
 
 class Soldier(pygame.sprite.Sprite):
-    def __init__(self, char_type, x, y, scale, speed, ammo,):
+    def __init__(self, char_type, x, y, scale, speed, ammo, grenades,):
         pygame.sprite.Sprite.__init__(self)
         self.alive = True
         self.char_type = char_type
@@ -50,6 +54,7 @@ class Soldier(pygame.sprite.Sprite):
         self.ammo = ammo
         self.start_ammo = ammo
         self.shoot_cooldown = 0
+        self.grenades = grenades
         self.health = 100
         self.max_health = self.health
         self.direction = 1
@@ -62,12 +67,12 @@ class Soldier(pygame.sprite.Sprite):
         self.action = 0
         self.update_time = pygame.time.get_ticks()
         
-        #load all images for the players
+        #ladda in all bilder för spelaren
         animation_types = ['Idle', 'Run', 'Jump', 'Death']
         for animation in animation_types:
-            #reset temporary list of images
+            #starta om den temporära listan av bilder
             temp_list = []
-            #count number of files in the folder
+            #räckna antalet av filerna i mappen
             num_of_frames = len(os.listdir(f'img/{self.char_type}/{animation}'))
             for i in range(num_of_frames):
                 img = pygame.image.load(f'img/{self.char_type}/{animation}/{i}.png').convert_alpha()
@@ -83,16 +88,16 @@ class Soldier(pygame.sprite.Sprite):
     def update(self):
         self.update_animation()
         self.check_alive()
-        #update cooldown
+        #uppdatera nedräckning
         if self.shoot_cooldown > 0:
             self.shoot_cooldown -= 1
 
     def move(self, moving_left, moving_right):
-        #reset movement variables
+        #starta om rörelse variablerna
         dx = 0
         dy = 0
 
-        #assign movement variables if moving left or right
+        #ge ut rörelse variablerna om man rör sig vänster eller höger
         if moving_left:
             dx = -self.speed
             self.flip = True
@@ -102,26 +107,26 @@ class Soldier(pygame.sprite.Sprite):
             self.flip = False
             self.direction = 1
 
-        #jump
+        #Hoppa
         if self.jump == True and self.in_air == False:
             self.vel_y = -11
             self.jump = False
             self.in_air = True
 
 
-        #Apply gravity
+        #Lägg till gravitation
         self.vel_y += GRAVITY
         if self.vel_y > 10:
             self.vel_y
         dy += self.vel_y 
 
-        #check collision with floor
+        #Kolla kollision med Golvet
         if self.rect.bottom + dy > 300:
             dy = 300 - self.rect.bottom
             self.in_air = False
 
 
-        #update rectangle position
+        #Uppdatera rectangelens position
         self.rect.x += dx
         self.rect.y += dy
 
@@ -130,20 +135,20 @@ class Soldier(pygame.sprite.Sprite):
             self.shoot_cooldown = 20
             bullet = Bullet(self.rect.centerx + (0.6 * self.rect.size[0] * self.direction), self.rect.centery, self.direction)
             bullet_group.add(bullet)
-            #reduce ammo
+            #Minska ammo
             self.ammo -= 1
             
 
     def update_animation(self):
-        #update animation
+        #Uppdatera animation
         ANIMATION_COOLDOWN = 100
-        #update image depending on current frame
+        #Uppdatera bilderna beroende på nuvarande frame
         self.image = self.animation_list[self.action][self.frame_index]
-        #check if enough time has passed since the last update
+        #Kolla om tillräckligt med tid har g¨tt sen förra uppdateringen
         if pygame.time.get_ticks() - self.update_time > ANIMATION_COOLDOWN:
             self.update_time = pygame.time.get_ticks()
             self.frame_index += 1
-        #if the animation has run out reset back to the start
+        #Om animationerna har tagit slut starta då om från starten
         if self.frame_index >= len(self.animation_list[self.action]):
             if self.action == 3:
                 self.frame_index = len(self.animation_list[self.action]) -1
@@ -153,10 +158,10 @@ class Soldier(pygame.sprite.Sprite):
 
 
     def update_action(self, new_action):
-        #check if the new action is different to the previous one
+        #Kolla om den nya aktivitet är anorlund från förra
         if new_action != self.action:
             self.action = new_action
-            #update the animation settings 
+            #uppdatera animations inställningarna 
             self.frame_index = 0
             self.update_time = pygame.time.get_ticks()
 
@@ -181,13 +186,13 @@ class Bullet(pygame.sprite.Sprite):
         self.direction = direction
 
     def update(self):
-        #move bullet
+        #Flytta på kulan
         self.rect.x += (self.direction * self.speed)
-        #check if bullet has gone off screen
+        #Kontrollera om kulan har försvunnit från skärmen
         if self.rect.right < 0 or self.rect.left > SCREEN_WIDTH:
             self.kill()
 
-        #check collision with characters
+        #Kolla kollision med karaktärer
         if pygame.sprite.spritecollide(player, bullet_group, False):
             if player.alive:
                 player.health -= 5
@@ -198,12 +203,44 @@ class Bullet(pygame.sprite.Sprite):
                 self.kill()
 
 
-#create sprite groups
+class Grenade(pygame.sprite.Sprite):
+    def __init__(self, x, y, direction):
+        pygame.sprite.Sprite.__init__(self)
+        self.timer = 100
+        self.vel_y = -11
+        self.speed = 7
+        self.image = grenade_img
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+        self.direction = direction
+
+    def update(self):
+        self.vel_y += GRAVITY
+        dx = self.direction * self.speed
+        dy = self.vel_y
+
+
+        #Kontrollera kollision med golvet
+        if self.rect.bottom + dy > 300:
+            dy = 300 - self.rect.bottom
+            self.speed = 0
+
+        #Kontrollera kollision med väggarna
+        if self.rect.left + dx < 0 or self.rect.right +dx > SCREEN_WIDTH:
+            self.direction *= -1
+            dx = self.direction * self.speed
+
+        #uppdatera grenatens position
+        self.rect.x += dx
+        self.rect.y += dy
+
+#Skapa sprite Grupper
 bullet_group = pygame.sprite.Group()
+grenade_group = pygame.sprite.Group()
 
 
-player = Soldier('player', 200, 200, 3, 5, 20)
-enemy = Soldier('enemy', 400, 200, 3, 5, 20)
+player = Soldier('player', 200, 200, 3, 5, 20, 5)
+enemy = Soldier('enemy', 400, 200, 3, 5, 20, 0)
 
 
 run = True
@@ -219,16 +256,26 @@ while run:
     enemy.update()
     enemy.draw()
 
-    #update and draw groups
+    #Uppdatera och rita ut grupperna
     bullet_group.update()
+    grenade_group.update()
     bullet_group.draw(screen)
+    grenade_group.draw(screen)
 
 
-    #update player actions
+    #Uppdatera spelarens händelser
     if player.alive:
-        #shoot bullets
+        #Skjut kula
         if shoot:
             player.shoot()
+        #Kasta Granater
+        elif grenade and grenade_thrown == False and player.grenades > 0:
+            grenade = Grenade(player.rect.centerx + (0.5 * player.rect.size[0] * player.direction),\
+                        player.rect.top, player.direction)
+            grenade_group.add(grenade)
+            #Minska antalet granater
+            player.grenades -= 1
+            grenade_thrown = True
         if player.in_air:
             player.update_action(2)#2: Jump
         elif moving_left or moving_right:
@@ -238,10 +285,10 @@ while run:
         player.move(moving_left, moving_right)
 
     for event in pygame.event.get():
-        #quit game
+        #Avsluta Spelet
         if event.type == pygame.QUIT:
             run = False
-        #keyboard presses
+        #Tangentbord aktiveringar
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_a:
                 moving_left = True
@@ -249,13 +296,15 @@ while run:
                 moving_right = True
             if event.key == pygame.K_SPACE:
                 shoot = True
+            if event.key == pygame.K_q:
+                grenade = True
             if event.key == pygame.K_w and player.alive:
                 player.jump = True
             if event.key == pygame.K_ESCAPE:
                 run = False
 
 
-        #keyboard button released
+        #Tangentbord knapp släppt
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_a:
                 moving_left = False
@@ -263,6 +312,9 @@ while run:
                 moving_right = False
             if event.key == pygame.K_SPACE:
                 shoot = False
+            if event.key == pygame.K_q:
+                grenade = False
+                grenade_thrown = False
 
 
 
